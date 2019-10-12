74a556921a6e75a128a2573ee8667ac5f007f729
Hentioe <me@bluerain.io>
2019-10-12 05:25:11 -0400

    Improved CityLive parameters reception

--- lib/community_web/live/city_live.ex
+++ lib/community_web/live/city_live.ex
@@ -8,30 +8,30 @@ defmodule CommunityWeb.CityLive do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, [])}
+    {:ok, socket}
   end
 
   def handle_params(params, _uri, socket) do
-    id = params |> Map.get("id", "0") |> to_i
+    id = to_i(params["id"] || "0")
+    {:noreply, socket |> assign(id: id) |> fetch}
+  end
+
+  def fetch(%Socket{assigns: %{id: id}} = socket) do
+    socket |> assign(users: find_users(id), city: find_city(id))
+  end
+
+  defp find_users(_id) do
+    1..45
+    |> Enum.map(fn i ->
+      %{id: i, name: "user-#{i}", face: "#{Enum.random(1..3)}.png"}
+    end)
+  end
+
+  defp find_city(id) do
     cities = IndexLive.cities()
 
-    cities =
-      cities
-      |> Enum.filter(fn city -> city.id == id end)
-
-    city =
-      if cities |> Enum.count() > 0 do
-        cities |> hd
-      else
-        nil
-      end
-
-    users =
-      1..45
-      |> Enum.map(fn i ->
-        %{id: i, name: "user-#{i}", face: "#{Enum.random(1..3)}.png"}
-      end)
-
-    {:noreply, socket |> assign(city: city, users: users)}
+    cities
+    |> Enum.filter(fn city -> city.id == id end)
+    |> hd
   end
 end
